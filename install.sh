fedba73d9d5eb9845f9d263b033b90004638b25d
Stefan Puchmann <sp@timeular.com>
2024-02-26 16:10:24 +0100

    initial tmux cfg

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@ mkdir ~/.config
 CUR_DIR=$(pwd)
 ln -s "$CUR_DIR/config/nvim" ~/.config/nvim
 ln -s "$CUR_DIR/ideavimrc" ~/.ideavimrc
+ln -s "$CUR_DIR/tmux.conf" ~/.tmux.conf
 source "$CUR_DIR/config/zsh/install.sh"
